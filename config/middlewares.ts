ea23223b985254b1128db3fdba209a5f51d66f8f
BruceBui5041 <brucebui5041@gmail.com>
2023-05-25 09:18:05 +0700

    Create blog page

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,27 @@
 export default [
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::logger',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        directives: {
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "https://dl.airtable.com",
+            "https://cdn.shopify.com",
+          ],
+        },
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
